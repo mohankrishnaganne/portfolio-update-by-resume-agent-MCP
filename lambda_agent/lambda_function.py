20ca5badfc2ee4440b731cad828132140b6e823d
Mohan Krishna Ganne <mohanganne99@gmail.com>
2026-07-28 20:37:30 -0500

    Updating for triggering new image from workflow

--- lambda_agent/lambda_function.py
+++ lambda_agent/lambda_function.py
@@ -12,6 +12,7 @@ def lambda_handler(event, context):
     print("Received S3 Event: ", json.dumps(event))
     
     try:
+        print("Welcome to My Portfolio Creator! 🚀")
         # 1. Parse bucket and filename from the S3 event trigger
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
